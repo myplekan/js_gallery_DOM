fe8dbdc0b5325f784474fb1dca5613c46f1d733c
Mykola Plekan <zikzar@gmail.com>
2024-01-19 18:59:15 +0200

    make build

---
+++ index.1123abf3.js
@@ -0,0 +1,3 @@
+var e=document.querySelector("#largeImg"),t=document.querySelector("#thumbs");t.addEventListener("click",function(r){r.preventDefault();var a=r.target.closest(".list-item__link");if(a&&t.contains(a)){var n="./"+a.attributes.href.nodeValue;e.attributes.src.nodeValue=n}});//# sourceMappingURL=index.1123abf3.js.map
+
+//# sourceMappingURL=index.1123abf3.js.map
